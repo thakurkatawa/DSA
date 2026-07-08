1404c8e8ffb567d4e709f3d757bab7d7c231baa6
thakurkatawa <155065619+thakurkatawa@users.noreply.github.com>
2026-07-08 17:51:39 +0400

    Time: 2 ms (92.17%) | Memory: 47.6 MB (87.98%) - LeetSync

--- 283-move-zeroes/move-zeroes.java
+++ 283-move-zeroes/move-zeroes.java
@@ -1,17 +1,13 @@
 class Solution {
     public void moveZeroes(int[] nums) {
+        int moves=0;
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]!=0){
+                int temp=nums[i];
+                nums[i]=nums[moves];
+                nums[moves]=temp;
 
-        int j = 0; // Position to place the next non-zero element
-
-        for (int i = 0; i < nums.length; i++) {
-
-            if (nums[i] != 0) {
-
-                int temp = nums[i];
-                nums[i] = nums[j];
-                nums[j] = temp;
-
-                j++;
+                moves++;
             }
         }
     }
