1bfdf9f9aba50e92b535fb4a32003d10a694c674
thakurkatawa <155065619+thakurkatawa@users.noreply.github.com>
2026-08-04 18:45:25 +0530

    Time: 0 ms (100.00%) | Memory: 19.2 MB (88.80%) - LeetSync

---
+++ 13-roman-to-integer/roman-to-integer.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    def romanToInt(self, s):
+        Map={
+        "I":1,
+        "V":5,
+        "X":10,
+        "L":50,
+        "C":100,
+        "D":500,
+        "M":1000,        
+        }
+        N=len(s)
+        i=N-1
+        output=0
+        while i>=0:
+            if i < N-1 and Map[s[i]] <  Map[s[i+1]]:
+                output -= Map[s[i]]
+            else:
+                output += Map[s[i]]
+            i=i-1
+        return output
