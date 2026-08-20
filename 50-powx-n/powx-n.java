a7755fc771cb16d454537bfe134ada2b37e87f90
thakurkatawa <155065619+thakurkatawa@users.noreply.github.com>
2026-08-21 01:59:55 +0530

    Time: 0 ms (100.00%) | Memory: 48.3 MB (11.55%) - LeetSync

--- 50-powx-n/powx-n.java
+++ 50-powx-n/powx-n.java
@@ -2,13 +2,10 @@ class Solution {
     public double myPow(double x, int n) {
         double ans = 1.0;
         long nn = n;
-
         if (nn < 0) {
-            nn = -nn;
+            nn=-nn;
         }
-
         while (nn > 0) {
-
             if (nn % 2 == 1) {
                 ans = ans * x;
                 nn = nn - 1;
@@ -17,9 +14,8 @@ class Solution {
                 nn = nn / 2;
             }
         }
-
-        if (n < 0) {
-            ans = 1.0 / ans;
+        if (n<0) {
+            ans=1.0/ans;
         }
 
         return ans;
